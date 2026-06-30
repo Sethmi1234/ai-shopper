505f1623c23ef0b843129b7f88cc93c0b750e530
Sethmi1234 <disethmi@gmail.com>
2026-06-30 11:03:38 +0530

    setup login module with auth structure

---
+++ src/hooks/mutations/useRefreshToken.ts
@@ -0,0 +1,12 @@
+import { useMutation } from "@tanstack/react-query";
+import { refreshToken } from "@/services/auth.service";
+
+export const useRefreshToken = () => {
+  return useMutation({
+    mutationFn: (refreshTokenValue: string) => refreshToken(refreshTokenValue),
+    onSuccess: (data) => {
+      localStorage.setItem("accessToken", data.accessToken);
+      localStorage.setItem("refreshToken", data.refreshToken);
+    },
+  });
+};
